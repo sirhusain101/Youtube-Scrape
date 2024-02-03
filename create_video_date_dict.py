dcaac3b7a9b9c9a05e5353a028456ea6e5095981
Muddassir H <Muddassirhusain98@gmail.com>
2024-02-03 23:22:30 +0530

    Delete create_video_date_dict.py

--- create_video_date_dict.py
+++
@@ -1,9 +0,0 @@
-id = []
-
-date = []
-
-print(dict(zip(id, date)))
-
-# IMPORTANT
-# Save the dictionary created here, in a separate file named - video_ids.py
-# Within this video_ids.py file, the variable for dictionary should be video_id_date_dict
