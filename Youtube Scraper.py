4da8df4c74a4ecd7a1533bb8a5ff182211b01a02
Muddassir H <Muddassirhusain98@gmail.com>
2024-02-03 23:04:17 +0530

    Update Youtube Scraper.py

--- Youtube Scraper.py
+++ Youtube Scraper.py
@@ -1,44 +1,12 @@
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 import requests
-import re
+import json
 import os
-from video_ids import video_id_date_dict
-
-
+from video_ids import video_id_list   # Check comments in create_video_date_dict.py for more info
 
 # Clears any text from Results.html, which was left after previous operation
 open("Results.html", "w", encoding="utf-8").close()
-
-# Table header
-with open("Results.html", "a", encoding="utf-8") as file:
-    file.write(tabulate([["S. No.", "Thumbnail", "Date", "Title", "Description"]], tablefmt='html'))
-
-for id, date in video_id_date_dict.items():
-    # Clears any text from python console window.
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    # Displays whichever video id is being being scraped
-    print("(" + str(list(video_id_date_dict).index(id) + 1) + "/" + str(len(video_id_date_dict)) + ") | Scraping: https://www.youtube.com/watch?v=" + id)
-
-    # Sends request for scraping
-    soup = BeautifulSoup(requests.get("https://www.youtube.com/watch?v=" + id).content, "html.parser")
-
-    # Scrapes title with regex
-    title_pattern = re.compile('(?<=title><meta name="title" content=").*(?="><meta name="description" content=)')
-    title = title_pattern.findall(str(soup))[0].replace('\\n','\n')
-
-    # Scrapes description with regex
-    description_pattern = re.compile('(?<=meta name="description" content=").*(?="><meta name="keywords" content=)')
-    description = description_pattern.findall(str(soup))[0].replace('\\n','\n')
-
-    # Saves scrapped data in table format
-    with open("Results.html", "a", encoding="utf-8") as file:
-            file.write(tabulate([[str(list(video_id_date_dict).index(id) + 1),
-                                 "<img src=\"thumbnails/" + id + ".jpg\" height=150 width=200>",
-                                 date,
-                                 "<a href=\"https://www.youtube.com/watch?v=" + id + "\" target=\"_blank\">" + title + "</a>",
-                                 description]], tablefmt='unsafehtml'))
 
 # Adds CSS to output file for better display
 with open("Results.html", "a", encoding="utf-8") as file:
@@ -55,9 +23,39 @@ table, th, td {
 }
 </style>""")
 
+# Table header
+with open("Results.html", "a", encoding="utf-8") as file:
+    file.write(tabulate([["S. No.", "Thumbnail", "Date", "Title", "Description"]], tablefmt='html'))
+
+# Main loop
+for index, id in enumerate(video_id_list):
+    # Clears any text from python console window.
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Displays whichever video id is being being scraped
+    print("(" + str(index + 1) + "/" + str(len(video_id_list)) + ") | Scraping: https://www.youtube.com/watch?v=" + id)
+
+    # Sends request for scraping
+    soup = BeautifulSoup(requests.get("https://www.youtube.com/watch?v=" + id).content, "lxml")
+
+    # Scrapes date | https://www.youtube.com/watch?v=XVv6mJpFOb0
+    date = soup.find('meta', itemprop="datePublished")['content']
+    # Scrapes title
+    title = soup.title.string
+    # Scrapes description | https://www.youtube.com/watch?v=QNLBBGWEQ3Q | https://jsonformatter.org | https://jsonformatter.org/json-parser
+    description_json = soup.find_all('script')[45].text.strip()[20:-1]
+    description = json.loads(description_json)['contents']['twoColumnWatchNextResults']['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['attributedDescription']['content']
+
+    # Saves scrapped data in table format
+    with open("Results.html", "a", encoding="utf-8") as file:
+            file.write(tabulate([[str(index + 1), "<img src=\"thumbnails/" + id + ".jpg\" height=150 width=200>",
+                                 date[:10],
+                                 "<a href=\"https://www.youtube.com/watch?v=" + id + "\" target=\"_blank\">" + title + "</a>",
+                                 description]], tablefmt='unsafehtml'))
+
 # Clears any text from python console window.
 os.system('cls' if os.name == 'nt' else 'clear')
 # Displays a message on python console once the process is done.
-print("All the videos were searched successfully! Please check Results.html file.")
+print("All the videos were scraped successfully! Please check Results.html file.")
 # Stop python console from closing automatically.
 input("Press ENTER key to exit!")
